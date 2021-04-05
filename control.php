ff23d2d5cf1801abcfb67296b85c331fb65c9f54
Dzyfhuba <hafidz21ub@gmail.com>
2021-04-05 15:28:29 +0700

    revisi navbar isActive

--- control.php
+++ control.php
@@ -1,12 +1,17 @@
 <?php
 
 $curPageName = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1);
-//     echo "The current page name is: ".$curPageName;  
-//     echo "</br>";
 
 $title = "Pretest Praktikum A2-Pagi";
 $home = "index.php";
 $portofolio = "portofolio.php";
 $about = "about.php";
+// echo $curPageName;
+// echo $home;
+function isActive($target){
+	if ($curPageName == $target) {
+		return " active";
+	}
+}
 
 ?>
