2c01e1b7fcef9bb29bb96e1aae8864287a32df3c
Dzyfhuba <hafidz21ub@gmail.com>
2021-04-05 13:08:27 +0700

    initial

---
+++ control.php
@@ -0,0 +1,10 @@
+<?php
+
+$curPageName = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1);
+//     echo "The current page name is: ".$curPageName;  
+//     echo "</br>";
+
+$title = "Pretest Praktikum A2-Pagi";
+$home = "index.php";
+$portofolio = "portofolio.php";
+$about = "about.php";
