22e463c16db1db2e376c2a474a78735e698e80c0
Dzyfhuba <hafidz21ub@gmail.com>
2021-04-26 11:38:56 +0700

    using post. next, another way

--- control.php
+++ control.php
@@ -9,11 +9,22 @@ $portofolio = "portofolio.php";
 $about = "about.php";
 // echo $curPageName;
 // echo $home;
-function isActive($target){
+function isActive($target)
+{
 	$currentRoute = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1);
 	if ($currentRoute == $target) {
 		echo " active";
 	}
 }
 
-?>
+// echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">';
+// echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>';
+// echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>';
+// echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>'
+
+
+// $jumlah_porto = 5;
+
+if (isset($_POST['jumlah_porto'])) {
+	$jumlah_porto = $_POST['jumlah_porto'];
+}
--- portofolio.php
+++ portofolio.php
@@ -9,27 +9,46 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo $title ?></title>
 	<link rel="stylesheet" href="style.css">
+	<script src="sript.js"></script>
 </head>
 
 <body>
 	<?php include "navbar.php" ?>
 	<h2 style="text-align: center;">List Portofolio</h2>
 	<div class="porto-content">
-		<?php for($i = 0; $i < 10; $i++) { ?>
-			<div class="porto-item">
-				<div class="porto-title"><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</b></div>
-				<div class="porto-desc">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</div>
-				<a class="button" href="#detail">Detail</a>
+		<form action="" method="post" name="set_jumlah_porto">
+			<div class="input-group mb-3">
+				<select id="jumlah_porto" class="custom-select col-5" name="jumlah_porto">
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5" selected="selected">5</option>
+					<option value="6">6</option>
+					<option value="7">7</option>
+					<option value="8">8</option>
+					<option value="9">9</option>
+					<option value="10">10</option>
+				</select>
+				<input type="submit" value="Sort" />
+		</form>
+	</div>
+	</form>
+	<?php for ($i = 0; $i < $jumlah_porto; $i++) { ?>
+		<div class="porto-item">
+			<div class="porto-title"><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</b></div>
+			<div class="porto-desc">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</div>
+			<a class="button" href="#detail">Detail</a>
+		</div>
 
-		<?php } ?>
+	<?php } ?>
 	</div>
 </body>
 
