84f25bec62c7440ab0212db08f23359ea119241f
Hafidz Ubaidillah <hafidz21ub@gmail.com>
2021-05-31 12:06:18 +0700

    Update about.php

--- about.php
+++ about.php
@@ -18,23 +18,28 @@
     <?php include "navbar.php" ?>
     <h2 style="text-align: center;">About</h2>
     <div class="profile">
-        <img class="profile-image" src=<?php echo "images/".$foto; ?> alt="avatar generic image" class="profile-photo">
+        <img class="profile-image" src=<?php echo "images/".$data[7]; ?> alt="avatar generic image" class="profile-photo">
         <div class="profile-text">
             <table>
                 <tr>
                     <td><b>Nama</b></td>
                     <td>: </td>
-                    <td><?php echo $nama; ?></td>
+                    <td><?php echo $data[0]; ?></td>
                 </tr>
                 <tr>
                     <td><b>Jenis Kelamin</b></td>
                     <td>: </td>
-                    <td><?php echo $jk; ?></td>
+                    <td><?php echo $data[1]; ?></td>
                 </tr>
                 <tr>
                     <td><b>Alamat</b></td>
                     <td>: </td>
-                    <td><?php echo $alamat.", ".$kecamatan.", ". $kabupaten.", ".$provinsi.", ".$kodepos; ?></td>
+                    <td><?php echo $data[2].", ".$data[3].", ". $data[4].", ".$data[5].", ".$data[6]; ?></td>
+                </tr>
+                <tr>
+                    <td><b>Umur</b></td>
+                    <td>: </td>
+                    <td><?php echo hitung_umur($data[8])[0]; ?></td>
                 </tr>
             </table>
         </div>
